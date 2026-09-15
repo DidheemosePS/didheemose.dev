3ee2517f62c7008883dc6c2f9d3ca9dad21ffa8c
DidheemosePS <sebastiandidheemose2002@gmail.com>
2026-09-15 13:56:40 +0100

    profile deatils and projects update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,8 @@ export default function Home() {
           </h1>
 
           <p className="mt-2 whitespace-nowrap text-sm font-medium sm:text-base">
-            {currentAge}yo Cloud Infrastructure Engineer from Ireland 🇮🇪
+            {currentAge}yo Software Engineer (Full-Stack & Cloud) from Ireland
+            🇮🇪
           </p>
 
           <p className="mt-4 max-w-sm text-balance text-sm sm:text-base">
